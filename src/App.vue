d9b638a4280b413777651ac0ffeb9a6b8261a0f3
Yarden Lawson <yardenlawson@gmail.com>
2021-02-23 17:01:55 +0000

    initial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar">
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      api_key: 'b88a85e254ae3645ecb15e045529b73f'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
